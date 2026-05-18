377b5dbe0e17f3353b13c584929cb1ed24010ada
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-18 21:28:37 +0100

    feat: add protect middleware to serviceRoutes

--- api/routes/serviceRoutes.js
+++ api/routes/serviceRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController');
+const { protect } = require('../middleware/auth');
 
 router.get('/', serviceController.getAllServices);
 router.get('/trader/:id', serviceController.getServicesByTrader);
 router.get('/:id', serviceController.getServiceById);
-router.post('/', serviceController.addService);
-router.put('/:id', serviceController.editService);
-router.delete('/:id', serviceController.deleteService);
+router.post('/', protect, serviceController.addService);
+router.put('/:id', protect, serviceController.editService);
+router.delete('/:id', protect, serviceController.deleteService);
 
 module.exports = router;
