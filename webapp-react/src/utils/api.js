c19b34f195c18915a62ef3f0b61573288d8a968f
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-18 23:21:03 +0100

    fix: fixing env vars issue

--- webapp-react/src/utils/api.js
+++ webapp-react/src/utils/api.js
@@ -1,3 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3002';
+const API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:3002'
+  : 'https://sole-traders.onrender.com';
 
 export default API_URL;
